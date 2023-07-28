97c16e40a95a49c474a5e5995a2869c9f78ac92f
TTLandwehr <114362508+TTLandwehr@users.noreply.github.com>
2023-07-27 21:57:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv("LA_Crimes.csv")
+df = pd.read_csv("LA_Crime.csv")
+
+st.set_page_config(page_title="StreamLit Example",layout="wide")
+
+with st.container():
+  st.write("Crimes that have happened in LA in 2020")
+  st.write("---------------------------------------")
